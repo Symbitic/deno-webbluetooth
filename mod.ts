7d864d5201192f242a0fadf2d5470f8e2481218c
Alex Shaw <alex.shaw.as@gmail.com>
2022-06-22 12:47:13 -0700

    feat: export device and gatt

--- mod.ts
+++ mod.ts
@@ -1,5 +1,7 @@
 export * from "./src/bluetooth.ts";
+export * from "./src/device.ts";
 export * from "./src/ffi.ts";
+export * from "./src/gatt.ts";
 export * from "./src/interfaces.ts";
 export * from "./src/symbols.ts";
 export { VERSION } from "./version.ts";
